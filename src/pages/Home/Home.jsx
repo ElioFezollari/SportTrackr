e6e310800fbd0c0868ef827fc122ec6288e399c3
ElioFezollari <fezollarielio@gmail.com>
2024-12-31 15:45:39 -0500

    Added phone page for landing home

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,5 +1,9 @@
 import React from 'react'
 import home from '../../assets/images/home/home-bg.webp'
+import homePhone from '../../assets/images/home/phone-main.png'
+import signupPhone from '../../assets/images/home/phone-signup.png'
+import leaguePhone from '../../assets/images/home/phone-league.png'
+import teamPhone from '../../assets/images/home/phone-team.png'
 import '../../styles/home.css'
 
 function Home() {
@@ -13,8 +17,30 @@ function Home() {
           <a className='btn b-white '>Try the app out!</a>
         </div>
         </div>
+        <div className='static-home-work'>
+          <h2>How SportTrackr works</h2>
+          <p>Join an organized soccer league and track everything as if youre a pro!</p>
+          <div className='home-phones'>
+            <div>
+              <h5>1. Create an account with us</h5>
+          <img src={signupPhone}  alt="" />
+          </div>
+          <div>
+          <h5>2. Pick a league of your choosing</h5>
+          <img src={leaguePhone} alt="" />
+          </div>
+          <div>
+            <h5>3. Join your team</h5>
+          <img src={teamPhone}  alt="" />
+          </div>
+          <div>
+            <h5>4. Enjoy our features</h5>
+          <img src={homePhone}  alt="" />
+          </div>
+          </div>
+        </div>
     </div>
   )
 }
 
-export default Home
+export default Home  
