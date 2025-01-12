ad1f8c6018410148f34f961657970ab1ab2fb595
ElioFezollari <fezollarielio@gmail.com>
2025-01-11 20:15:46 -0500

    Made Login Page Responsive And Decorated Login Image Section

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,6 +1,11 @@
 import React from 'react'
 import '../../styles/login.css'
 import soccerPlayer from "../../assets/images/login/login-player.svg"
+import shoes from "../../assets/images/login/shoes.svg"
+import trophy from "../../assets/images/login/trophy.svg"
+import whistle from "../../assets/images/login/whistle.svg"
+import ball from "../../assets/images/login/soccer-ball.svg"
+import field from "../../assets/images/login/field.svg"
 import logo from "../../assets/images/Logo3.png"
 import { Link } from 'react-router'
 function Login() {
@@ -17,7 +22,14 @@ function Login() {
             <a href="">Forgot Password?</a>
             <p>Don' t have an account? <Link href="">Sign Up</Link></p>
               </div>
-            <div className='login-image'><img src={soccerPlayer} alt="player kicking ball" /></div>
+            <div className='login-image'>
+              <img src={soccerPlayer} alt="player kicking ball" />
+              <img className='absolute' src={shoes} alt="image of a pair of shoes" />
+              <img className='absolute' src={trophy} alt="image of a trophy" />
+              <img className='absolute' src={whistle} alt="image of a whistle" />
+              <img className='absolute' src={ball} alt="image of a soccer ball" />
+              <img className='absolute' src={field} alt="image of a field" />
+              </div>
         </div>
     </div>
   )
