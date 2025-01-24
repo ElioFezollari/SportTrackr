910b7e34e724a9c973e5f678eb471c4721adc365
issakat <101413112@georgebrown.ca>
2025-01-24 03:28:34 -0500

    Edited Footer

--- src/components/layout/staticlayout/StaticFooter.jsx
+++ src/components/layout/staticlayout/StaticFooter.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import '../../../styles/components/layouts/staticFooter.css';
-import logo from '../../../assets/images/Logo3.png';
 import { Link, NavLink } from 'react-router-dom';
+import logo from '../../../assets/images/Logo3.png';
 
 const currentYear = new Date().getFullYear();
 
@@ -9,17 +9,39 @@ function StaticFooter() {
     return (
         <div className='static-footer'>
             <footer>
-                <div className='footer-install-banner'>
-                    <h2>Try SportTrackr for free!</h2>
-                    <div>
-                        <a className='btn b-gray'>Install Our App</a>
-                        <Link to="" className='btn b-gray'>Create an account</Link>
+                <div className="footer-right">
+                    <div className="social-media-links">
+                        <a href="https://www.facebook.com" target="_blank" rel="noopener noreferrer" aria-label="Facebook">
+                            <i className="fa-brands fa-facebook"></i>
+                        </a>
+                        <a href="https://www.instagram.com" target="_blank" rel="noopener noreferrer" aria-label="Instagram">
+                            <i className="fa-brands fa-instagram"></i>
+                        </a>
+                        <a href="https://www.twitter.com" target="_blank" rel="noopener noreferrer" aria-label="Twitter">
+                            <i className="fa-brands fa-twitter"></i>
+                        </a>
+                        <a href="https://www.youtube.com" target="_blank" rel="noopener noreferrer" aria-label="YouTube">
+                            <i className="fa-brands fa-youtube"></i>
+                        </a>
+                        <a href="https://www.tiktok.com" target="_blank" rel="noopener noreferrer" aria-label="TikTok">
+                            <i className="fa-brands fa-tiktok"></i>
+                        </a>
                     </div>
                 </div>
+
+                <hr />
+
                 <div className="static-footer-wrapper">
+
                     <div className="static-footer-image">
                         <img src={logo} alt="SportTrackr Logo"/>
                         <h1>SportTrackr</h1>
+
+                        <div className="footer-contact-info">
+                            <p>160 Kendal Avenue</p>
+                            <p>Toronto, ON M5R 1M3</p>
+                            <p>Phone: (416) 415-2005</p>
+                        </div>
                     </div>
 
                     <div className="static-footer-link">
@@ -30,39 +52,22 @@ function StaticFooter() {
                         <NavLink activeClassName="is-active" to={"/privacy-policy"}>Privacy Policy</NavLink>
                         <NavLink activeClassName="is-active" to={"/terms-and-conditions"}>Terms & Conditions</NavLink>
                     </div>
-                </div>
 
-                <div className="footer-right">
-                    <div className="follow-us">
-                        <h2>Follow Us:</h2>
-                    </div>
-
-                    <div className="social-media-links">
-                        <a href="https://www.facebook.com" target="_blank" rel="noopener noreferrer"
-                           aria-label="Facebook">
-                            <i className="fa-brands fa-facebook"></i>
-                        </a>
-                        <a href="https://www.instagram.com" target="_blank" rel="noopener noreferrer"
-                           aria-label="Instagram">
-                            <i className="fa-brands fa-instagram"></i>
-                        </a>
-                        <a href="https://www.twitter.com" target="_blank" rel="noopener noreferrer"
-                           aria-label="Twitter">
-                            <i className="fa-brands fa-twitter"></i>
-                        </a>
-                        <a href="https://www.youtube.com" target="_blank" rel="noopener noreferrer"
-                           aria-label="YouTube">
-                            <i className="fa-brands fa-youtube"></i>
-                        </a>
+                    <div className="footer-install-banner">
+                        <h2>Try SportTrackr for free!</h2>
+                        <div className="install-buttons">
+                            <a className="btn b-gray">Install Our App</a>
+                            <Link to="" className="btn b-gray">Create an account</Link>
+                        </div>
                     </div>
                 </div>
             </footer>
-            <div className='copyright-footer'>
+
+            <div className="copyright-footer">
                 <h4>© SportTrackr {currentYear}</h4>
             </div>
         </div>
-    )
-        ;
+    );
 }
 
 export default StaticFooter;
