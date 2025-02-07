7263d18abb5198f73c175be0b1cf758691ffa191
Aum Zaveri <aumzaveri06@gmail.com>
2025-02-07 10:40:09 -0500

    OnBoarding Added

---
+++ src/services/onboarding.js
@@ -0,0 +1,18 @@
+import axios from 'axios'
+const baseUrl = 'http://localhost:5000/v1/connect'
+
+const getOnboardingUrl = async (token) => {
+    try {
+        const response = await axios.post(baseUrl, null, {
+            headers: {
+                Authorization: `Bearer ${token}`
+            },
+            withCredentials: true 
+        });
+        return response;
+    } catch (e) {
+        console.error("Error fetching URL:", e);
+    }
+}
+
+export { getOnboardingUrl }
