6a624e9b21ace9078e860f22a3b78375eab9490d
Truong Thuy Ky Duyen <81848246+duyen2310@users.noreply.github.com>
2025-03-27 16:55:26 -0400

    chang port

--- src/services/transaction.js
+++ src/services/transaction.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-const baseUrl = 'http://localhost:5001/v1/transaction'
+const baseUrl = 'http://localhost:5000/v1/transaction'
 
 const getLeagueOwnerTransactions = async (token)=>{
     const config = {
